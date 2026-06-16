c3be465e66efd986509100e96dc7f8e7e249187c
A-Light-Shining-in-Darkness <kor2002rea@naver.com>
2026-06-16 10:23:23 +0900

    037

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -254,6 +254,31 @@ public class ApiV1PostControllerTest {
 
 
     @Test
+    @DisplayName("글 작성, without authorization header")
+    void t10() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": "내용"
+                                        }
+                                        """)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.resultCode").value("401-1"))
+                .andExpect(jsonPath("$.msg").value("로그인 후 이용해주세요."));
+    }
+
+
+    @Test
     @DisplayName("글 다건조회")
     void t5() throws Exception {
         ResultActions resultActions = mvc
